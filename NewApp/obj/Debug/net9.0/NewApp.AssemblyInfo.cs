b19cf4881a03a1e4c1d723d012b2e8ece82b2ce0
MDuyen03 <myduyen051203@gmail.com>
2025-01-12 17:54:23 +0700

    commit not

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41dff0e341267d9dcf5a4ccc893303c3aeacc07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff07067e36e3f59ce6d83c955651603b807df3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
